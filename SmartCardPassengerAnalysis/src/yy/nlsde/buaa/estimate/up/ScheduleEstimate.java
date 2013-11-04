9dd68b5a40a10d4b0ba8744e9307517dc59b7362
mapleque@hp <mapleque@gmail.com>
2013-11-04 16:34:35 +0800

    method implement

--- SmartCardPassengerAnalysis/src/yy/nlsde/buaa/estimate/up/ScheduleEstimate.java
+++ SmartCardPassengerAnalysis/src/yy/nlsde/buaa/estimate/up/ScheduleEstimate.java
@@ -1,15 +1,30 @@
 package yy.nlsde.buaa.estimate.up;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.HashMap;
 import java.util.List;
 
 public class ScheduleEstimate {
+	public static void main(String[] args){
+		ScheduleEstimate est=new ScheduleEstimate();
+		est.estimate();
+		est.saveSchedule();
+	}
+	
+	public static final int TRIP_MAX=30*60;//trip threshold (s)
+	public static final int STOP_MAX=5*60;//stop threshold (s)
 
-	private HashMap<String, List<Object>> lineSchedule;
+	private List<String> schedule;//<line_vehicle_station_time>
+
+	private HashMap<String, List<String>> lineSchedule;
 
 	private HashMap<String, List<String>> entryOnlyLineDetail;
+
+	private boolean isScheduleGenraled = false;
 	
-	private boolean isEntryOnlyScheduleGenraled=false;
+	private boolean isScheduleFixed=false;
 
 	public void estimate() {
 		CardBean card = null;
@@ -18,10 +33,13 @@ public class ScheduleEstimate {
 	}
 
 	public void saveSchedule() {
-		if (!isEntryOnlyScheduleGenraled){
-			generalEntryOnlySchedule();
+		if (!isScheduleGenraled) {
+			generalSchedule();
 		}
-		//TODO: save the schedule
+		if(isScheduleFixed){
+			fixSchedule();
+		}
+		// TODO: save the schedule
 	}
 
 	private void studyCard(CardBean card) {
@@ -29,26 +47,132 @@ public class ScheduleEstimate {
 			// entry only model
 			String line = card.getLineId();
 			String time = card.getUpTime();
-			addEntryOnlyDetail(line, time);
+			String vehicle = card.getVehicleId();
+			addEntryOnlyDetail(line, vehicle, time);
 		} else {
 			// distance model
 			String line = card.getLineId();
 			String time = card.getDownTime();
 			String sta = card.getDownSeq();
-			updateDistanceSchedule(line, time, sta);
+			String vehicle = card.getVehicleId();
+			updateDistanceSchedule(line, vehicle, time, sta);
 		}
 	}
 
-	private void addEntryOnlyDetail(String line, String time) {
-		// TODO:add the detail to the entry only detail list
+	private void addEntryOnlyDetail(String line, String vehicle, String time) {
+		// add the detail to the entry only detail list
+		if (entryOnlyLineDetail == null) {
+			entryOnlyLineDetail = new HashMap<String, List<String>>();
+		}
+		String key = line + "_" + vehicle;
+		if (!entryOnlyLineDetail.containsKey(key)) {
+			entryOnlyLineDetail.put(key, new ArrayList<String>());
+		}
+		entryOnlyLineDetail.get(key).add(time);
 	}
 
-	private void updateDistanceSchedule(String line, String time, String sta) {
-		// TODO:update the distance model line schedule
+	private void updateDistanceSchedule(String line, String vehicle,
+			String time, String station) {
+		// update the distance model line schedule
+		if (lineSchedule == null) {
+			lineSchedule = new HashMap<String, List<String>>();
+		}
+		String key = line + "_" + vehicle + "_" + station;
+		if (!lineSchedule.containsKey(key)) {
+			lineSchedule.put(key, new ArrayList<String>());
+		}
+		lineSchedule.get(key).add(time);
 	}
 
-	public void generalEntryOnlySchedule() {
-		isEntryOnlyScheduleGenraled=true;
-		//TODO:general the entry only line schedule by detail
+	public void generalSchedule() {
+		// general the entry only line schedule by detail
+		schedule = new ArrayList<String>();
+		if (entryOnlyLineDetail != null && !entryOnlyLineDetail.isEmpty()) {
+			// entry only line schedule build
+			for (String key : entryOnlyLineDetail.keySet()) {
+				List<String> times = entryOnlyLineDetail.get(key);
+				Collections.sort(times, new Comparator<String>() {
+					@Override
+					public int compare(String o1, String o2) {
+						try {
+							return Integer.parseInt(o1) - Integer.parseInt(o2);
+						} catch (Exception e) {
+						}
+						return 0;
+					}
+				});
+				String oldtime=null;
+				String timestamp="";
+				int stationstamp=1;
+				int sum=0;
+				int num=0;
+				for (String time:times){
+					if (oldtime==null){
+						oldtime=time;
+						continue;
+					}
+					try {
+						int range= Integer.parseInt(time) - Integer.parseInt(oldtime);
+						if (range>TRIP_MAX){
+							String newkey=key+"_"+stationstamp;
+							if (num > 0) {
+								sum /= num;
+							}
+							timestamp = Integer.toString(sum);
+							schedule.add(newkey+"_"+timestamp);
+							sum=0;num=0;
+							stationstamp=1;
+						}else if (range>STOP_MAX){
+							String newkey=key+"_"+stationstamp;
+							if (num > 0) {
+								sum /= num;
+							}
+							timestamp = Integer.toString(sum);
+							schedule.add(newkey+"_"+timestamp);
+							sum=0;num=0;
+							stationstamp++;
+						}else{
+							sum += Integer.parseInt(time);
+							num++;
+						}
+					} catch (Exception e) {
+						System.err.println("time format error:" + time
+								+ " or " + oldtime);
+						continue;
+					}
+					oldtime=time;
+				}
+			}
+		}
+		if (lineSchedule != null && !lineSchedule.isEmpty()) {
+			// common schedule build
+			for (String key : lineSchedule.keySet()) {
+				List<String> times = lineSchedule.get(key);
+				String timestamp = "";
+				int sum = 0;
+				int num = 0;
+				for (String time : times) {
+					// average the times
+					try {
+						sum += Integer.parseInt(time);
+						num++;
+					} catch (Exception e) {
+						System.err.println("time format error:" + time);
+						continue;
+					}
+				}
+				if (num > 0) {
+					sum /= num;
+				}
+				timestamp = Integer.toString(sum);
+				schedule.add(key+"_"+timestamp);
+			}
+		}
+		isScheduleGenraled = true;
+	}
+	
+	public void fixSchedule(){
+		//TODO:fix the schedule;
+		isScheduleFixed=true;
 	}
 }
--- SmartCardPassengerAnalysis/src/yy/nlsde/buaa/estimate/up/UpEstimate.java
+++ SmartCardPassengerAnalysis/src/yy/nlsde/buaa/estimate/up/UpEstimate.java
@@ -1,44 +1,111 @@
 package yy.nlsde.buaa.estimate.up;
 
-public class UpEstimate {
-	
-	private Object lineSchedule;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
-	public void estimate(String date){
+public class UpEstimate {
+	public static void main(String[] args){
+		UpEstimate es=new UpEstimate();
+		es.estimate("20130905");
+	}
+
+	private HashMap<String, List<String>> staMap;// <line_vehicle,list<station_time>>
+	private HashMap<String, List<String>> timeMap;// <line_vehicle_staion,list<time>
+
+	public void estimate(String date) {
 		this.lineScheduleReadIn(date);
 		CardBean card = null;
 		// TODO:card read in
 		this.estimate(card);
-		//TODO:save card
+		// TODO:save card
 	}
-	
-	private void lineScheduleReadIn(String date){
-		// TODO:read in the line schedule
+
+	private void lineScheduleReadIn(String date) {
+		// read in the line schedule
+		staMap=new HashMap<String,List<String>>();
+		timeMap=new HashMap<String,List<String>>();
+		String line="";//line_vehicle_station_time
+		
+		while(true){//TODO:read in each line
+			String[] data=line.split("_");
+			String stakey=data[0]+"_"+data[1];
+			String stavalue=data[2]+"_"+data[3];
+			if (!staMap.containsKey(stakey)){
+				staMap.put(stakey, new ArrayList<String>());
+			}
+			staMap.get(stakey).add(stavalue);
+			
+			String timekey=data[0]+"_"+data[1]+"_"+data[2];
+			String timevalue=data[3];
+			if (!timeMap.containsKey(timekey)){
+				timeMap.put(timekey, new ArrayList<String>());
+			}
+			timeMap.get(timekey).add(timevalue);
+		}
 	}
-	
-	private void estimate(CardBean card){
+
+	private void estimate(CardBean card) {
 		if (card.isEntryOnly()) {
 			// entry only model
 			String line = card.getLineId();
 			String time = card.getUpTime();
-			String sta=getUpStation(line,time);
+			String vehicle = card.getVehicleId();
+			String sta = getUpStation(line, vehicle, time);
 			card.setUpSeq(sta);
 		} else {
 			// distance model
 			String line = card.getLineId();
 			String sta = card.getUpSeq();
-			String time=getUpTime(line, sta);
+			String vehicle = card.getVehicleId();
+			String dtime = card.getDownTime();
+			String time = getUpTime(line, vehicle, sta, dtime);
 			card.setUpTime(time);
 		}
 	}
-	
-	private String getUpStation(String line,String time){
-		//TODO: get the up station from the schedule
-		return null;
+
+	private String getUpStation(String line, String vehicle, String time) {
+		// get the up station from the schedule
+		String sta = "-1";
+		String key = line + "_" + vehicle;
+		if (staMap.containsKey(key)) {
+			List<String> list = staMap.get(key);
+			int oldrange = -1;
+			for (String sta_time : list) {
+				String[] data = sta_time.split("_");
+				//sta_time=station_time
+				String ststa = data[0];
+				String sttime = data[1];
+				int range = Math.abs(Integer.parseInt(time)
+						- Integer.parseInt(sttime));
+				if (oldrange < 0) {
+					oldrange = range;
+				}
+				if (range < ScheduleEstimate.STOP_MAX && range <= oldrange) {
+					sta = ststa;
+				}
+			}
+		}
+		return sta;
 	}
-	
-	private String getUpTime(String line,String seq){
-		//TODO: get the up time from the schedule
-		return null;
+
+	private String getUpTime(String line, String vehicle, String seq,
+			String dtime) {
+		String utime="-1";
+		String key=line+"_"+vehicle+"_"+seq;
+		if (timeMap.containsKey(key)){
+			List<String> list=timeMap.get(key);
+			int oldrange = -1;
+			for (String time:list){
+				int range=Integer.parseInt(dtime)-Integer.parseInt(time);
+				if (oldrange < 0) {
+					oldrange = range;
+				}
+				if(range>0&&range<=oldrange){
+					utime=time;
+				}
+			}
+		}
+		return utime;
 	}
 }
